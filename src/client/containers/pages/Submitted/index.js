3b415192c653dd6ffe2c00295bf767a68df18c81
joaker <jooakley@gmail.com>
2017-06-23 19:30:41 -0400

    include submitted screen

---
+++ src/client/containers/pages/Submitted/index.js
@@ -0,0 +1,15 @@
+import './index.scss';
+import Section from 'components/Section';
+import Card from 'components/Card';
+
+export const Submitted = () => (
+			<Section id="join">
+				<Card>
+				<p>
+					To apply to join the Baobab Table Supper Club, provide your email address and a short introduction.
+					If a peanut could kill you, please mention that.  We'll wait for a peanut-free night to invite you.
+				</p>
+				</Card>
+			</Section>
+);
+export default Submitted;
